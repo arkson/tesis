8b3284c4ba0c67413118e1fb2060df030a47f088
Arcadio Pernia <arkson888@gmail.com>
2012-05-19 19:46:24 -0430

    cambios 19/05/12

--- app/mailers/clave_mailer.rb
+++ app/mailers/clave_mailer.rb
@@ -2,7 +2,7 @@ class ClaveMailer < ActionMailer::Base
   #default :from => "from@example.com"
 
   def olvido_clave(usuario)  
-	mail(:to => usuario.correo, :subject => "Envio de clave", :from => "kennysoytupadre@gmail.com")  
+	mail(:to => usuario.correo, :subject => "Nueva Clave de Inicio de Sesión", :from => "bolsadelibro")  
   end   
 
 end
