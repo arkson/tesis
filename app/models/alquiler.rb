6208524a9a2e94dd7fb5ca7513e93a8e7faac4fd
Kenny Capote <kenny.capote@gmail.com>
2012-05-23 01:08:54 -0430

    toma en cuenta los lubros alquilados

--- app/models/alquiler.rb
+++ app/models/alquiler.rb
@@ -27,15 +27,11 @@ def total_price
 end
 
 
-def total_ejemplares(usuario_id) 
-	prealquilado = 'Prealquilado'
-	alquilado = 'Alquilado'
-	#line_item.count(:all)
-	#line_item.count(:conditions => ' alquiler.usuario_id = :usuario_id AND ejemplar.estatus_ejemplar = :prealquilado OR ejemplar.estatus_ejemplar = :alquilado ', {:prealquilado => prealquilado, :alquilado => alquilado, :usuario_id => usuario_id }   )	
 
- 	
-	return 0
-
+def total_libros
+	
+	line_item.to_a.count
+	
 
 end	
 
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,5 @@
 class Usuario < ActiveRecord::Base
-
+  has_many :alquiler
   belongs_to :dependencia
   validates :cedula, :presence => true, :uniqueness => true
   has_secure_password
