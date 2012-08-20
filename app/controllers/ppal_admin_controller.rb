b81af42e77458e2e5526bf5d76e067fc5525148d
Kenny Capote <kenny.capote@gmail.com>
2012-08-20 16:59:46 -0430

    graficos

--- app/controllers/ppal_admin_controller.rb
+++ app/controllers/ppal_admin_controller.rb
@@ -7,11 +7,11 @@ class PpalAdminController < ApplicationController
   add_breadcrumb "Inicio", :ppal_admin_index_path
   
   def index
-  @line_chart = Gchart.line(:data => [0, 40, 10, 70, 20])
-  
+#theme => keynote, thirty7signals, pastel, greyscale   
 
-  #Libros alquilados por escuela
+###Libros alquilados agrupados por area de conocimiento
 
+	
   @ejemplares_area = Ejemplar.find_by_sql("select count(e.id) as cantidad, b.area_conocimiento_id, c.nombre
 											from alquileres a join lines_items l on (a.id = l.alquiler_id)
 											join devoluciones d on (l.id = d.line_item_id)
@@ -21,18 +21,25 @@ class PpalAdminController < ApplicationController
 											where a.estatus = 'Alquilado'
 											and d.estatus = 'Sin devolver'
 											group by b.area_conocimiento_id")	
+# #{user.name} (#{total == 0 ? 0 : (100.0 * number / total).round}%)"
 
-   
+  	 total = @ejemplares_area.to_a.sum{|ejem| ejem.cantidad}   
 	 @datos = Array.new
 	 @label = Array.new
 	 @legend = Array.new
  	 @ejemplares_area.each do |area|
-		@datos << area.cantidad	
-		@label << area.nombre + " #{area.cantidad}" 
-		@legend << area.nombre
+		@datos << area.cantidad
+		porc = area.cantidad * 100 / total	
+		@label << area.nombre + " %#{porc} " 
+		@legend << area.nombre + " (#{area.cantidad})"
 	 end	
 	 
-      @pie_chart1 = Gchart.pie_3d(:data => @datos, :title => "Libros alquilados por area de conocimiento", :labels => @label, :legend => @legend, :theme => :thirty7signals,:size => '500x150')	
+      @pie_chart1 = Gchart.pie_3d(:data => @datos, :title => "Libros alquilados por area de conocimiento", :labels => @label, :legend => @legend, :theme => :thirty7signals,:size => '550x150')	
+
+
+##################################################################################################################
+#Libros alquilados agrupados por escuela de alumno solicitante
+
 
 
 	 @ejemplares_escuela = Ejemplar.find_by_sql("select count(e.id) as cantidad, u.dependencia_id, c.nombre
@@ -44,18 +51,51 @@ class PpalAdminController < ApplicationController
 													where a.estatus = 'Alquilado'
 													and d.estatus = 'Sin devolver'
 													group by c.id") 
-	
+	 total = @ejemplares_escuela.to_a.sum{|ejem| ejem.cantidad} 
 	 @datos = Array.new
 	 @label = Array.new
 	 @legend = Array.new
  	 @ejemplares_escuela.each do |escuela|
 		@datos << escuela.cantidad	
-		@label << escuela.nombre + " #{escuela.cantidad}" 
-		@legend << escuela.nombre
+		porc = escuela.cantidad * 100 / total	
+		@label << escuela.nombre + " %#{porc} " 
+		@legend << escuela.nombre + " (#{escuela.cantidad})"
 	 end	
 
 	
-	 @pie_chart2 = Gchart.pie_3d(:data => @datos, :title => "Ejemplares entregados a cada escuela", :labels => @label, :legend => @legend, :theme => :thirty7signals,:size => '500x150')	
+	 @pie_chart2 = Gchart.pie_3d(:data => @datos, :title => "Ejemplares entregados a cada escuela", :labels => @label, :legend => @legend, :theme=>:pastel, :size => '550x150')	
+
+
+##################################################################################################################
+#Libros alquilados agrupados por usuarios de las escuelas
+
+
+     @usuarios_escuela = Ejemplar.find_by_sql("select count(distinct(u.id)) as cantidad, u.dependencia_id, c.nombre
+												from alquileres a join lines_items l on (a.id = l.alquiler_id)
+												join devoluciones d on (l.id = d.line_item_id)
+												join ejemplares e on (l.ejemplar_id = e.id)
+												join usuarios u on (a.usuario_id = u.id)
+												join dependencias c on (u.dependencia_id = c.id )
+												where a.estatus = 'Alquilado'
+												and d.estatus = 'Sin devolver'
+												group by c.id")
+
+	 total = @usuarios_escuela.to_a.sum{|usu| usu.cantidad} 
+	 @datos = Array.new
+	 @label = Array.new
+	 @legend = Array.new
+ 	 @usuarios_escuela.each do |usuario|
+		@datos << usuario.cantidad	
+		porc = usuario.cantidad * 100 / total	
+		@label << usuario.nombre  + " %#{porc} " 
+		@legend << usuario.nombre + " (#{usuario.cantidad})"
+	 end	
+
+	
+	 @pie_chart3 = Gchart.pie_3d(:data => @datos, :title => "Ejemplares entregados a cada escuela", :labels => @label, :legend => @legend, :theme => :greyscale,:size => '550x150')	
+
+
+
   end
 
 
